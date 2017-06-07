31738117540e080841deb46384447786ac941e68
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-06-07 08:44:08 -0700

    Install navigator and set up base screens

---
+++ src/screens/ReviewScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ReviewScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Boilerplate Text</Text>
+        <Text>Review Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default ReviewScreen;
---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SettingsScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Boilerplate Text</Text>
+        <Text>Settings Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default SettingsScreen;
